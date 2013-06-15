1a67d2c16832c04b3ccb27927558dfa47f0ae752
Rodrigo Ulisses <rodrigo.ulisses7@gmail.com>
2013-06-15 16:38:40 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProvalabWeb::Application.config.secret_token = '66eb1164f888c3f343d33a8b18685de2033871103389384da17198c69eae7162e403e51e657c05116591037a49fd1c23c2a0c533e622368366640bc206309924'
